6b7c997022c4682e84abee52c6b4dd000356449e
parseltongist <alex.milinkevich@gmail.com>
2021-07-11 20:31:53 +0200

    stage 2/4 draft 2.1

--- handler.py
+++ handler.py
@@ -20,7 +20,7 @@ def sort_list():
         sort_list()
 
 sizes = []
-full_data = []
+# full_data = []
 
 if len(args) < 2:  # 2 here because counting starts from 0, and args[0] is the name of a python3 script
     print("Directory is not specified")
@@ -28,19 +28,21 @@ else:
     folder = args[1]
 
     format = input("Enter file format:\n")
-    if format:
-        format = "." + format
+    #if format:
+        #format = "." + format
 
     for root, dirs, files in os.walk('.', topdown=True):
         if len(format) > 0:
             for name in files:
-                if str(os.path.splitext(name)[1]) == format:
+                if str(os.path.splitext(name)[1]) == format[1:]:
                     sizes.append(os.path.getsize(os.path.join(root, name)))
                 sizes = list(set(sizes))
+                sizes = [str(x) for x in sizes]  # test str
         else:
             for name in files:
                 sizes.append(os.path.getsize(os.path.join(root, name)))
             sizes = list(set(sizes))
+            sizes = [str(x) for x in sizes]  # test str
 
     sort_list()
     test_dict = dict.fromkeys(sizes, [])
@@ -49,18 +51,25 @@ else:
         for name in files:
             full_path = os.path.join(root, name)
             size = os.path.getsize(full_path)
-            full_data.append([full_path, size])
+            # full_data.append([full_path, size])
+            for x in test_dict:
+                if str(os.path.getsize(full_path)) == x:
+                    test_dict[x].append(os.path.join(root, name))
+                    # can make sort of lists in dict
+                    # issue is that filter doesn't work. all files are appended to all dict keys
 
-    for x in test_dict:
-        for y in full_data:
-            test_dict[x] = [y[0] for y in full_data if str(y[1]) == str(x)]
+    #for x in test_dict:
+    #    for y in full_data:
+    #        test_dict[x] = [y[0].lower() for y in full_data if str(y[1]) == str(x)]
 
     for x in test_dict:
         if str(x) != '0':
             print(int(x) * 1000, "bytes")
             for y in test_dict[x]:
-                print(y)
+                print(y if len(y) > 2 else "error")
             print()
 
-    #print(full_data)
-    #print('a')
+
+
+#print(os.path.getsize('./module/root_folder/calc/server.php'))
+print(os.path.getsize("./handler.py"))
