062a83a8bc3fe513f60c6ef55836143d8125421a
parseltongist <alex.milinkevich@gmail.com>
2021-07-19 19:57:25 +0200

    Stage 2/2 draft 2.2.

--- handler.py
+++ handler.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-args = sys.argv
+#print(os.path.getsize("./module/root_folder/calc/server.php"))
 
 def sort_list(sizes):
     sort_by = int(input("""Size sorting options:
@@ -10,10 +10,13 @@ def sort_list(sizes):
 2. Ascending\n"""))
     if sort_by == 1:
         sizes = sorted(sizes, reverse=True)
-        sizes = [str(x) for x in sizes]
+        sizes = [str(x) for x in sizes]  # changing value type int --> str only after sorting
+        pass
     elif sort_by == 2:
         sizes = sorted(sizes, reverse=False)
-        sizes = [str(x) for x in sizes]
+        sizes = [str(x) for x in sizes]  # changing value type int --> str only after sorting
+        #print(sizes)  # test print
+        pass
     else:
         print("\nWrong option")
         sort_list(sizes)
@@ -21,59 +24,66 @@ def sort_list(sizes):
 
 def print_results(test_dict):
     for x in test_dict:
-        if str(x) != '0':
-            print(int(x) * 1000, "bytes")
+        if len(test_dict[x]) > 0:
+            #print(int(x) * 1000, "bytes")
+            print(int(x), "kilobytes")
             for y in test_dict[x]:
-                print(y if len(y) > 2 else "error")
+                print(y)
+            #for y in test_dict[x]:
+            #    if len(y) > 2:
+            #        print(y)
             print()
 
 
 def check_directory():
+    args = sys.argv
     if len(args) < 2:  # 2 here because counting starts from 0, and args[0] is the name of a python3 script
         print("Directory is not specified")
     else:
         return main()
 
 
+def full_data_filter(lst, format):  # rename variable full_data to sizes
+    if format != "":
+        new_list = [x for x in lst if format in os.path.splitext(x)[1][1:]]
+    else:
+        new_list = [x for x in lst if os.path.splitext(x)[1][1:] != ""]
+    print("")
+
+    # test code
+    #for x in new_list:
+    #    print(x)
+    return new_list
+
+
 def main():
-    folder = args[1]
     format = input("Enter file format:\n")
     sizes = []
-    for root, dirs, files in os.walk('.', topdown=True):
-        if len(format) > 0:
-            for name in files:
-                if str(os.path.splitext(name)[1]) == format[1:]:
-                    sizes.append(os.path.getsize(os.path.join(root, name)))
-                sizes = list(set(sizes))
-                sizes = [str(x) for x in sizes]  # test str
-        else:
-            for name in files:
-                sizes.append(os.path.getsize(os.path.join(root, name)))
-            sizes = list(set(sizes))
-            sizes = [str(x) for x in sizes]  # test str
-
-    sort_list(sizes)
-    test_dict = dict.fromkeys(sizes, [])
-
+    full_data = []
     for root, dirs, files in os.walk('.', topdown=True):
         for name in files:
             full_path = os.path.join(root, name)
-            size = os.path.getsize(full_path)
-            # full_data.append([full_path, size])
-            for x in test_dict:
-                if str(os.path.getsize(full_path)) == x and x in test_dict:
-                    test_dict[x].append(os.path.join(root, name))
-                    # can make sort of lists in dict
-                    # issue is that filter doesn't work. all files are appended to all dict keys
+            full_data.append(full_path)
 
-    #for x in test_dict:
-    #    for y in full_data:
-    #        test_dict[x] = [y[0].lower() for y in full_data if str(y[1]) == str(x)]
+            #full_data = list(filter(lambda x: os.path.splitext(x[0])[1] == format, full_data))
+            #print(full_data)
+            #print(extension)
+    new_list = full_data_filter(full_data, format)  # filtering the list + returning new list, + assigning it to variable
+    sizes = [os.path.getsize(x) for x in new_list]
+    sizes = list(set(sizes))
+    #print(sizes)
+    sort_list(sizes)  # sorting list with data.
+    # creating dict using sizes list as keys
+    test_dict = dict.fromkeys(sizes, [])
 
+    for x in test_dict:
+        test_dict[x] = [y.lower() for y in new_list if str(x) == str(os.path.getsize(y))]
+
+
+
+    #print(test_dict)
     print_results(test_dict)
 
 
-check_directory()
-
-#print(os.path.getsize('./module/root_folder/calc/server.php'))
-#print(os.path.getsize("./handler.py"))
+if __name__ == "__main__":  # running program only if it's called directly, not imported
+   check_directory()
