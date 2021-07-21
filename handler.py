2b5e1194238c1716e544f23c323786bcc08aac56
parseltongist <alex.milinkevich@gmail.com>
2021-07-21 21:44:35 +0200

    stage 2/2 working clean code.

--- handler.py
+++ handler.py
@@ -3,6 +3,28 @@ import os
 import sys
 
 
+def check_directory():
+    args = sys.argv
+    if len(args) < 2:  # 2 here because counting starts from 0, and args[0] is the name of a python3 script
+        print("Directory is not specified")
+        exit()
+    else:
+        pass
+
+
+def obtaining_data_list(file_extension):
+    lst = []
+    for root, dirs, files in os.walk('.', topdown=True):
+        for name in files:
+            lst.append(os.path.join(root, name))
+    if file_extension != "":
+        lst = [x for x in lst if file_extension in os.path.splitext(x)[1][1:]]
+    else:
+        lst = [x for x in lst if os.path.splitext(x)[1][1:] != ""]
+    print("")
+    return lst
+
+
 def sort_list(sizes):
     sort_by = int(input("""Size sorting options:
 1. Descending
@@ -18,6 +40,15 @@ def sort_list(sizes):
         sort_list(sizes)
 
 
+def creating_sorted_dict(data_list):
+    file_sizes = sort_list(list(set(os.path.getsize(x) for x in data_list)))
+    paths_dict = dict.fromkeys(file_sizes, [])
+    # paths_dict = dict.fromkeys(sort_list(list(set(os.path.getsize(x) for x in data_list))), [])
+    for x in paths_dict:
+        paths_dict[x] = [y for y in data_list if str(x) == str(os.path.getsize(y))]
+    return paths_dict
+
+
 def print_results(paths_dict):
     print()
     for x in paths_dict:
@@ -30,46 +61,12 @@ def print_results(paths_dict):
     print()
 
 
-def check_directory():
-    args = sys.argv
-    if len(args) < 2:  # 2 here because counting starts from 0, and args[0] is the name of a python3 script
-        print("Directory is not specified")
-    else:
-        return main()
-
-
-def full_data_filter(lst, file_format):
-    if file_format != "":
-        lst = [x for x in lst if file_format in os.path.splitext(x)[1][1:]]
-    else:
-        lst = [x for x in lst if os.path.splitext(x)[1][1:] != ""]
-    print("")
-    return lst
-
-
-def creating_dict(full_data):
-    file_sizes = sort_list(list(set(os.path.getsize(x) for x in full_data)))  # sorting list with data.
-    paths_dict = dict.fromkeys(file_sizes, [])
-    for x in paths_dict:
-        paths_dict[x] = [y for y in full_data if str(x) == str(os.path.getsize(y))]
-    return paths_dict
-
-
-def obtaining_data_list():
-    full_data = []
-    for root, dirs, files in os.walk('.', topdown=True):
-        for name in files:
-            full_data.append(os.path.join(root, name))
-    return full_data
-
-
 def main():
-    file_format = input("Enter file file_format:\n")
-    full_data = obtaining_data_list()
-    full_data = full_data_filter(full_data, file_format)  # filtering the list + returning new list, + assigning it to variable
-    # creating dict using sizes list as keys
-    paths_dict = creating_dict(full_data)
+    check_directory()
+    full_data = obtaining_data_list(input("Enter the file format:\n"))
+    paths_dict = creating_sorted_dict(full_data)
     print_results(paths_dict)
 
+
 if __name__ == "__main__":
-    check_directory()
+    main()
