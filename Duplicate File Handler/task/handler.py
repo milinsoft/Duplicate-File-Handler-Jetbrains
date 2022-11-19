093f5a08ca165b2afe5bf2ff4c3d98fd3331b757
Aleksander Milinkevich <amilinkevich@trilab.pl>
2022-11-19 17:36:35 +0100

    Duplicate file handler in OOP.

--- Duplicate File Handler/task/handler.py
+++ Duplicate File Handler/task/handler.py
@@ -2,6 +2,9 @@ import os
 from sys import argv as args
 from hashlib import md5
 from os.path import getsize, join
+from colorama import Fore, init
+
+init(autoreset=True)
 
 
 class File:
@@ -11,7 +14,7 @@ class File:
         self.path = path
         self.hash = None
 
-    def get_hash(self) -> hash:
+    def compute_md5_hash(self) -> hash:
         """returns file's md5-hash value in hex-digit format"""
         with open(self.path, "rb") as file:
             self.hash = md5(file.read()).hexdigest()
@@ -20,20 +23,29 @@ class File:
 class DuplicateFileHandler:
 
     def __init__(self):
-        self.files_list: list = []
+        self.files_tuple: tuple = tuple()
         self.sort_option: bool or None = None
         self.result_dict: dict = {}
 
-    def get_files_list(self, extension=None):
+    def get_files_catalogue(self, extension=None):
         """recursively scans directory provided and creates the list of absolute paths filtering by extension"""
-        abs_paths_lst = [
+        #
+        abs_paths_tuple = tuple(
             join(root, name)
             for root, dirs, files in os.walk(r"./module/", topdown=True)
             for name in files
             if not extension or name.endswith(extension)
-        ]
+        )
 
-        self.files_list = [File(size=getsize(path), path=path) for path in abs_paths_lst]
+        file_sizes = tuple(getsize(path) for path in abs_paths_tuple)
+        self.files_tuple = tuple(
+            File(size=getsize(path), path=path) for path in abs_paths_tuple if file_sizes.count(getsize(path)) > 1
+        )
+
+        # calculating hashes only for potential duplicates
+        for file in self.files_tuple:
+            file.compute_md5_hash()
+
 
     def get_sorting_preference(self):
         reverse_status = {1: True, 2: False}
@@ -42,91 +54,103 @@ class DuplicateFileHandler:
             print("\nWrong option")
             return self.get_sorting_preference()
         self.sort_option = reverse_status[sort_option]
+        # sort for correct final print
+        self.files_tuple = tuple(
+            sorted(self.files_tuple, key=lambda f: (f.size, f.hash), reverse=self.sort_option)
+        )
 
-    def print_search_results(self):
-        # filling out self.result_dict: dict
-        for file in self.files_list:
-            if file.size not in self.result_dict:
-                self.result_dict[file.size] = [file.path]
-            else:
-                self.result_dict[file.size].append(file.path)
+    def print_same_size_files(self):
+        file_size = None
+        for file in self.files_tuple:
+            # making sure size is printed only once
+            if file_size != file.size:
+                file_size = file.size
+                print(f"\n{file_size:_} bytes\n")
+            print(file.path)
 
-        self.result_dict = dict(sorted(self.result_dict.items(), reverse=self.sort_option))
-        for size, path_list in self.result_dict.items():
-            if path_list:
-                print(f"{size} bytes\n{os.linesep.join(path_list)}")  # os.linesep is workaround to use '\n' in f string
+    def get_duplicate_files(self):
+        # removing objects with unique hash from the list as those are definitely not a duplicate!
+        file_hashes = tuple(file.hash for file in self.files_tuple)
+        self.files_tuple = tuple(filter(lambda x: file_hashes.count(x.hash) > 1, self.files_tuple))
+
 
     def check_for_duplicates(self):
         check = input("\nCheck for duplicates? (yes, no)\n").lower()
         if check == "yes":
-            files_size_list = sorted((file.size for file in self.files_list), reverse=self.sort_option)
+            files_size_list = sorted((file.size for file in self.files_tuple), reverse=self.sort_option)
             sizes_with_several_files = [x for x in files_size_list if files_size_list.count(x) > 1]
-            self.files_list = [file for file in self.files_list if file.size in sizes_with_several_files]
+            self.files_tuple = [file for file in self.files_tuple if file.size in sizes_with_several_files]
             
             # calculate hashes
-            for file in self.files_list:
-                file.get_hash()
+            for file in self.files_tuple:
+                file.compute_md5_hash()
 
     def print_duplicate_files(self):
         """this function prints the output(result) of the program. if a format key: \n values"""
+
+        # removing objects with unique hash from the list as those are definitely not a duplicate!
+        file_hashes = tuple(file.hash for file in self.files_tuple)
+        self.files_tuple = tuple(filter(lambda x: file_hashes.count(x.hash) > 1, self.files_tuple))
+
+
         del_dict = {}
-        self.result_dict = {}
-        for f in self.files_list:
-            if f.size not in self.result_dict:
-                self.result_dict[f.size] = {f.hash: [f.path]}
-            else:
-                if f.hash not in self.result_dict[f.size]:
-                    self.result_dict[f.size].update({f.hash: [f.path]})
-                else:
-                    if f.path not in self.result_dict[f.size][f.hash]:
-                        self.result_dict[f.size][f.hash].append(f.path)
+        file_size = file_hash = None
+        for i, file in enumerate(self.files_tuple, start=1):
+            # making sure size is printed only once
+            if file_size != file.size:
+                file_size = file.size
+                print(f"\n{file_size:_} bytes\n")
+            if file_hash != file.hash:
+                file_hash = file.hash
+                print("Hash:", file_hash)
 
-        size_hash_dictionary = dict(sorted(self.result_dict.items(), reverse=self.sort_option))
-
-        i = 1
+            print(f"{i}.", file.path)
+            del_dict[i] = file
+            i += 1
         print()
-        for file_size, file_hashes in size_hash_dictionary.items():
-            print()
-            print(file_size, "bytes\n")
-            for file_hash in file_hashes:
-                if len(file_hashes[file_hash]) > 1:
-                    print("Hash:", file_hash)
-                    for file_path in file_hashes[file_hash]:
-                        print(i, file_path)
-                        del_dict[i] = file_path
-                        i += 1
+        if del_dict:
+            return DuplicateFileHandler.delete_files(del_dict)
+        else:
+            msg = "No duplicates found"
+            exit(
+                print(
+                    f"{Fore.GREEN}{msg}!"
+                    if not self.file_extension
+                    else f"{Fore.GREEN}{msg} with file extension {Fore.RED}{self.file_extension} {Fore.GREEN}!"
+                )
+            )
 
-        print()
-        return self.delete_files(del_dict)
-
-    def delete_files(self, dl_dict) -> int:
-        decision = input("Delete files? (yes/no)")
-        if decision.lower() not in {"yes", "no"}:
+    @staticmethod
+    def delete_files(dl_dict):
+        decision = input("Delete files? (YES/NO): ")
+        while decision.lower() not in {"yes", "no"}:
             print("Wrong option")
-            return self.delete_files(dl_dict)
+            return DuplicateFileHandler.delete_files(dl_dict)
 
-        elif decision.lower() == "yes":
+        if decision.lower() == "yes":
             while True:
                 file_numbers = input("Enter file numbers to delete separated by space:\n").split(" ")
                 try:
                     file_numbers = sorted([int(number) for number in file_numbers], reverse=True)
+                    # checking the biggest index if it exists - all other too!
                     if file_numbers[0] not in dl_dict.keys():
                         raise ValueError
                 except ValueError:
-                    print("Wrong format")
+                    print("Wrong option")
                 else:
                     freed_space = 0
                     for number in file_numbers:
-                        freed_space += int(getsize(dl_dict[number]))
-                        os.remove(dl_dict[number])
-                    print(f"Total freed up space: {freed_space} bytes")
+                        freed_space += dl_dict[number].size
+                        os.remove(dl_dict[number].path)
+                    print(f"{Fore.GREEN}Total freed up space: {freed_space:_} bytes")
                     break
 
     def start(self):
         """this is the main function which calls other functions in a defined order"""
-        self.get_files_list(input("Enter the file format or press 'return' for all:\n"))
+        self.get_files_catalogue(input("Enter the file format or press 'return' for all:\n"))
         self.get_sorting_preference()
-        self.print_search_results()
+        self.print_same_size_files()
+        self.get_duplicate_files()
         self.check_for_duplicates()
         self.print_duplicate_files()
 
